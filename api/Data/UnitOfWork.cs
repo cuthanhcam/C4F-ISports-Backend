39c8da7cb0ec71d881174002ad00f57b20ccbf3b
cuthanhcam <cuthanhcam04@gmail.com>
2025-05-13 08:47:04 +0700

    fix: modified UnitOfWork - handling exceptions on constructor

--- api/Data/UnitOfWork.cs
+++ api/Data/UnitOfWork.cs
@@ -1,12 +1,80 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using api.Interfaces;
+using api.Repositories;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace api.Data
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
-        
+        private readonly ApplicationDbContext _context;
+        private bool _disposed = false;
+        private IDbContextTransaction? _transaction;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IGenericRepository<T> Repository<T>() where T : class
+        {
+            return new GenericRepository<T>(_context);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+            _transaction = await _context.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            if (_transaction == null)
+            {
+                throw new InvalidOperationException("No transaction is active.");
+            }
+            await _transaction.CommitAsync();
+            await _transaction.DisposeAsync();
+            _transaction = null;
+        }
+
+        public async Task RollbackTransactionAsync()
+        {
+            if (_transaction == null)
+            {
+                throw new InvalidOperationException("No transaction is active.");
+            }
+            await _transaction.RollbackAsync();
+            await _transaction.DisposeAsync();
+            _transaction = null;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    if (_transaction != null)
+                    {
+                        _transaction.Dispose();
+                        _transaction = null;
+                    }
+                    _context.Dispose();
+                }
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
