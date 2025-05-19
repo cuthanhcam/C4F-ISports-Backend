1a416b810c1788998f412a39b954f607491a45c0
cuthanhcam <cuthanhcam04@gmail.com>
2025-05-19 11:53:08 +0700

    docs(Models): Modified Models-v2.0.0 - add CloudinarySettings.cs

--- docs/Models-v2.0.0.cs
+++ docs/Models-v2.0.0.cs
@@ -696,3 +696,18 @@ namespace api.Models
         public ICollection<SearchHistory> SearchHistories { get; set; } = new List<SearchHistory>();
     }
 }
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class CloudinarySettings
+    {
+        public string CloudName { get; set; } = string.Empty;
+        public string ApiKey { get; set; } = string.Empty;
+        public string ApiSecret { get; set; } = string.Empty;
+    }
+}
